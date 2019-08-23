4f3e6eb0ad273b2a30841c94a332ae0cc1d424da
David Coelho <a21250080@alunos.isec.pt>
2019-08-23 18:37:37 +0100

    Updates

---
+++ precache-manifest.9e1735cf725cb423d8b23c3577d88a27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61683b0323657b55f3d858dd21323857",
+    "url": "/E-Commerce-Page/index.html"
+  },
+  {
+    "revision": "edb7c0f2799a6aa472b2",
+    "url": "/E-Commerce-Page/static/css/main.9b868176.chunk.css"
+  },
+  {
+    "revision": "5ea31f1f30a7ffdf207b",
+    "url": "/E-Commerce-Page/static/js/2.53b63a6f.chunk.js"
+  },
+  {
+    "revision": "edb7c0f2799a6aa472b2",
+    "url": "/E-Commerce-Page/static/js/main.438dcd17.chunk.js"
+  },
+  {
+    "revision": "1afd6330b22e590f7ae4",
+    "url": "/E-Commerce-Page/static/js/runtime~main.f9958a7e.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/E-Commerce-Page/static/media/logo.ee7cd8ed.svg"
+  }
+]);
